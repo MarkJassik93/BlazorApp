09eac0378ec2a8ff7e940905e8da1e758e47db34
THINKPAD-T490\markj <markjassik93@gmail.com>
2021-04-14 16:16:43 +0300

    Binding select element with database data in Blazor

--- BlazorApplication/BlazorTutorial/EmployeeManagement.Web/Startup.cs
+++ BlazorApplication/BlazorTutorial/EmployeeManagement.Web/Startup.cs
@@ -27,9 +27,12 @@ namespace EmployeeManagement.Web
             {
 
                 client.BaseAddress = new Uri("https://localhost:44313/");
+            });
+            services.AddHttpClient<IDepartmentService, DepartmentService>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44379/");
 
             });
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
