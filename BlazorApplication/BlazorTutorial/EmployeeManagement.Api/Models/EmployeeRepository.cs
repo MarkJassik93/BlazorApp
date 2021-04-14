be5628522cca0fe8c09800895728272fbc9be5af
THINKPAD-T490\markj <markjassik93@gmail.com>
2021-04-14 09:28:25 +0300

    SearchMethod lisamine

--- BlazorApplication/BlazorTutorial/EmployeeManagement.Api/Models/EmployeeRepository.cs
+++ BlazorApplication/BlazorTutorial/EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Api.Models
@@ -50,6 +51,24 @@ namespace EmployeeManagement.Api.Models
         public async Task<IEnumerable<Employee>> GetEmployees()
         {
             return await appDbContext.Employees.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> Search(string name, Gender? gender)
+        {
+            IQueryable<Employee> query = appDbContext.Employees;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(e => e.FirstName.Contains(name)
+                            || e.LastName.Contains(name));
+            }
+
+            if (gender != null)
+            {
+                query = query.Where(e => e.Gender == gender);
+            }
+
+            return await query.ToListAsync();
         }
 
         public async Task<Employee> UpdateEmployee(Employee employee)
